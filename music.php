f5aea908e9c4f98c7f29b58b1dc4f5e51237c73a
Ziyosh <ziyodaakhmedova1998@gmail.com>
2020-02-29 15:28:05 +0300

    done

---
+++ music.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://localhost/music.php?playlist=mypicks.txt">
+	<head>
+		<title>Music Viewer</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<link href="viewer.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		<div id="header">
+			<h1>190M Music Playlist Viewer</h1>
+			<h2>Search Through Your Playlists and Music</h2>
+		</div>
+		
+		
+		<div id="listarea">
+			<ul id="musiclist">
+
+				        <li class="mp3item">
+							<a href="songs/190M Rap.mp3">
+								190M Rap.mp3</a>(58 b)
+						</li>
+						
+						
+						<li class="mp3item">
+							<a href="songs/Be More.mp3">
+								Be More.mp3	</a>(5.19 mb)
+						</li>
+
+						
+						<li class="mp3item">
+							<a href="songs/Drift Away.mp3">
+								Drift Away.mp3							</a>
+							(5.46 mb)
+						</li>
+
+						
+						<li class="mp3item">
+							<a href="songs/Hello.mp3">
+								Hello.mp3							</a>
+							(1.78 mb)
+						</li>
+
+						
+						<li class="mp3item">
+							<a href="songs/Just Because.mp3">
+								Just Because.mp3							</a>
+							(4.47 mb)
+						</li>
+
+						
+						<li class="mp3item">
+							<a href="songs/Panda Sneeze.mp3">
+								Panda Sneeze.mp3							</a>
+							(58 b)
+						</li>
+
+						
+						<li class="playlistitem">
+							<a href="http://localhost/songs/190M%20Mix.txt">
+								190M Mix.txt							</a>
+						</li>
+
+						
+						<li class="playlistitem">
+							<a href="http://localhost/songs/mypicks.txt">
+								mypicks.txt							</a>
+						</li>
+
+						
+						<li class="playlistitem">
+							<a href="http://localhost/songs/playlist.txt">
+								playlist.txt							</a>
+						</li>
+
+									</ul>
+		</div>
+	</body>
+</html>
